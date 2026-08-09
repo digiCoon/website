c53cc032c2cc647dfe0969ca84418919ec0deb69
digiCoon <203313163+digiCoon@users.noreply.github.com>
2026-08-09 16:21:42 +0200

    Add Legal page (Impressum & Datenschutz) with routing, favicon, robots.txt and sitemap.xml

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
+import { Routes, Route} from "react-router-dom";
 import Hero from './components/Hero'
 import Skills from './components/Skills'
 import Experience from './components/Experience'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
+import Legal from './components/Legal'
 
 function App() {
   return (
-      <>
-        <Hero />
-        <Skills />
-        <Projects />
-        <Experience />
-        <Contact />
-        <Footer />
-        </>
+      <Routes>
+          <Route path="/" element={
+             <>
+                 <Hero />
+                 <Skills />
+                 <Projects />
+                 <Experience />
+                 <Contact />
+                 <Footer />
+             </>
+          } />
+          <Route path="/impressum" element={<Legal />} />
+      </Routes>
   )
 }
 
