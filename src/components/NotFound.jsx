c6e42a0d5ac267dbdb0940b9d079b0e3c9c02240
digiCoon <203313163+digiCoon@users.noreply.github.com>
2026-08-12 10:16:17 +0200

    Add 404 page with custom illustration

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom'
+import raccoonTrash from '../assets/raccoon-trash-404.png'
+import './NotFound.css'
+
+function NotFound() {
+    return (
+        <section id="not-found">
+            <div>
+                <h1>404</h1>
+                <p>diese seite hat sich in den mülleimer verkrochen.</p>
+                <Link to="/">zurück zur startseite</Link>
+            </div>
+            <img src={raccoonTrash} alt="" className="not-found-image" />
+        </section>
+    )
+}
+
+export default NotFound
